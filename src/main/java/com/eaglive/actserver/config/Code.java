22f51ed0598d882e5945887fc33d0d77774d1e82
heliyu_sub <heliyu_sub@163.com>
2016-01-12 10:34:14 +0800

    delete

--- src/main/java/com/eaglive/actserver/config/Code.java
+++
@@ -1,8 +0,0 @@
-package com.eaglive.actserver.config;
-
-/**
- * Created by admin on 2015/11/12.
- */
-public class Code {
-    public static final int OK = 0;
-}
--- src/main/java/com/eaglive/actserver/message/response/ResponseMessage.java
+++
@@ -1,9 +0,0 @@
-package com.eaglive.actserver.message.response;
-
-/**
- * Created by admin on 2015/11/9.
- */
-public class ResponseMessage {
-    public String cmd;
-    public int code;
-}
