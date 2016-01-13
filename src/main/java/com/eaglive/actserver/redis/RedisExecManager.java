e062e109fdea42496eae3af69520186200026c38
heliyu_sub <heliyu_sub@163.com>
2016-01-13 13:25:13 +0800

    add redis

---
+++ src/main/java/com/eaglive/actserver/redis/RedisExecManager.java
@@ -0,0 +1,17 @@
+package com.eaglive.actserver.redis;
+
+/**
+ * Created by Administrator on 16-1-13.
+ */
+public class RedisExecManager {
+
+    private static final RedisExecManager _instance = new RedisExecManager();
+
+    private static final RedisExecManager instance(){
+        return _instance;
+    }
+
+    public void init(){
+
+    }
+}
